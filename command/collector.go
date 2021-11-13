9cc29a9d7367447d74b143f06a4068156df09d40
Adipa Wijayathilaka <adipamihi@gmail.com>
2021-11-13 18:08:59 -0500

    Use named return values in message handler

--- command/collector.go
+++ command/collector.go
@@ -22,21 +22,26 @@ func (h *MessageCollector) Description() string {
 	return "Collects messages starting from a given message ID to a given limit to local storage for further processing"
 }
 
-func (h *MessageCollector) Handle(s *discordgo.Session, m *discordgo.MessageCreate, args []string) (bool, error) {
+func (h *MessageCollector) Handle(s *discordgo.Session, m *discordgo.MessageCreate, args []string) (processedMessage bool, err error) {
 	if args[0] != "/collect" {
-		return false, nil
+		return
 	}
 
+	processedMessage = true
+
 	if len(args) != 3 {
-		return true, errors.New("invalid arguments")
+		err = errors.New("invalid arguments")
+		return
 	}
 
 	numMessagesToCollect, err := strconv.ParseUint(args[2], 10, 32)
 
 	if err != nil {
-		return true, fmt.Errorf("unable to parse value for number of messages to collect: %v", err)
+		err = fmt.Errorf("unable to parse value for number of messages to collect: %v", err)
+		return
 	}
 
 	print(numMessagesToCollect)
 	panic("implement me")
+	return
 }
--- handler.go
+++ handler.go
@@ -5,5 +5,5 @@ import "github.com/bwmarrin/discordgo"
 type Handler interface {
 	Name() string
 	Description() string
-	Handle(s *discordgo.Session, m *discordgo.MessageCreate, args []string) (bool, error)
+	Handle(s *discordgo.Session, m *discordgo.MessageCreate, args []string) (processedMessage bool, err error)
 }
